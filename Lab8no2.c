107c3b43ca5f948b81189032240417e03a11c751
6704062610119 <s6704062610119@email.kmutnb.ac.th>
2026-02-14 02:59:31 +0700

    commit3
    show number of no.1 stat

--- Lab8no2.c
+++ Lab8no2.c
@@ -1,8 +1,9 @@
-#include <stdio.h> //finish till commit2
-int checkscore(char std[]);
+#include <stdio.h> 
+int checkscore(char std[],char key[]);
+int firstright(char std[][10],char key[],int n);
 
 int main() {
-    int i=0;
+    int i=0,right=0;
     char ans[8][10]={
         	{'A','B','A','C','C','D','E','E','A','D'},//7<---std1
 			{'D','B','A','B','C','A','E','E','A','D'},//6<---std2
@@ -12,22 +13,37 @@ int main() {
 			{'B','B','E','C','C','D','E','E','A','D'},//7<---std6
 			{'B','B','A','C','C','D','E','E','A','D'},//7<---std7
 			{'E','B','E','C','C','D','E','E','A','D'}};//7<---std8
+	
+	char keys[10]={'D','B','D','C','C','D','A','E','A','D'};
 			
-	for(i=0;i<8;i++){
-		printf("std %d => %d\n", (i+1), checkscore(ans[i]));
+	for(i=0;i<8;i++){ //2.1
+		printf("std %d => %d\n", (i+1), checkscore(ans[i],keys));
 	}
+	
+	printf("No.1 correct: %d\n",firstright(ans,keys,8)); //2.2
+
 	
 	return 0;		
 }
 
-int checkscore(char std[]){
-	char keys[10]={'D','B','D','C','C','D','A','E','A','D'};
+int checkscore(char std[],char key[]){ //2.1
 	int i,score=0;
 	for(i=0;i<10;i++){
-		if(keys[i]==std[i]){
+		if(key[i]==std[i]){
 			score++;
 		}
 	}
 	return score;
 };
+
+int firstright(char std[][10],char key[],int n){ //2.2
+	int i,count=0;
+	for(i=0;i<n;i++){
+		if(std[i][0] == key[0]){
+			count++;
+		}
+	}
+	return count;
+};
+
 
